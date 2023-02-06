9e7f2ad7a2613a80294b40c63979702bca250170
Thinkpad <korotkiy.o@yandex.ru>
2023-02-07 00:33:42 +0300

    JpaRepository

--- src/main/java/ru/websocketserver/repository/DeviceDataJpaRepository.java
+++ src/main/java/ru/websocketserver/repository/DeviceDataJpaRepository.java
@@ -3,6 +3,10 @@ package ru.websocketserver.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.websocketserver.domain.entity.DeviceData;
 
+import java.util.Optional;
+
 public interface DeviceDataJpaRepository extends JpaRepository<DeviceData, Long> {
+
+    Optional<DeviceData> findDeviceDataByMac(String mac);
 
 }
